ee179de1497ec68c945ccb53314b9edadf63e3c0
deepfriedfilth <nkawatski@gmail.com>
2020-02-18 15:24:36 -0600

    Add hotfixes for desired search bar focus styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+var search = document.querySelector('nav form');
+var searchInput = search.querySelector('input');
+var searchButton = search.querySelector('button');
+
 var tigOle = document.querySelector('#tig-ole-bitties');
 var tigOleL = tigOle.querySelector('#lefty');
 var tigOleR = tigOle.querySelector('#righty');
+
+searchButton.addEventListener('click', keepFocus);
+search.addEventListener('mouseleave', removeFocus);
+
+function keepFocus(e) {
+
+	if(!searchInput.classList.contains('focused')) {
+		searchInput.classList.add('focused');
+	}
+
+}
+
+function removeFocus(e) {
+
+	if(e.target.nodeName.toLowerCase() == 'form') {
+		searchInput.classList.remove('focused');
+	}
+	
+}
