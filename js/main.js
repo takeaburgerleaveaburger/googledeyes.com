f722ab04ecd5e02db51b286e3b2a5a0da0725a62
deepfriedfilth <nkawatski@gmail.com>
2020-02-19 19:53:16 -0600

    Wobble baby, wobble baby..

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ var searchButton = search.querySelector('button');
 var tigOle = document.querySelector('#tig-ole-bitties');
 var tigOleL = tigOle.querySelector('#lefty');
 var tigOleR = tigOle.querySelector('#righty');
+var tigOleLPup = tigOleL.querySelector('.pupil');
+var tigOleRPup = tigOleR.querySelector('.pupil');
+
+document.addEventListener('mouseenter', eyesFollow);
+document.addEventListener('mouseleave', wobbleBaby);
 
 searchButton.addEventListener('click', keepFocus);
 search.addEventListener('mouseleave', removeFocus);
@@ -22,5 +27,26 @@ function removeFocus(e) {
 	if(e.target.nodeName.toLowerCase() == 'form') {
 		searchInput.classList.remove('focused');
 	}
-	
+
+}
+
+function eyesFollow(e) {
+	// eyes follow mouse/touch positions code here
+}
+
+function wobbleBaby(e) {
+	tigOleL.classList.remove('wobble');
+	tigOleR.classList.remove('wobble');
+
+	tigOleLPup.classList.remove('wobble');
+	tigOleRPup.classList.remove('wobble');
+
+	setTimeout(function() {
+		tigOleL.classList.add('wobble');
+		tigOleR.classList.add('wobble');
+
+		tigOleLPup.classList.add('wobble');
+		tigOleRPup.classList.add('wobble');
+	}, 100);
+}
 }
